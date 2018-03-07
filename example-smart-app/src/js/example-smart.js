28d98cd5e5553d02e27645f2509a6abed3d75237
Umakk <37061283+Umakk@users.noreply.github.com>
2018-03-07 16:27:19 +0530

    Removed alert msgs

--- example-smart-app/src/js/example-smart.js
+++ example-smart-app/src/js/example-smart.js
@@ -14,8 +14,8 @@ var launchContextId = getUrlParameter("launch");
 //var serviceUri = "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca";   //iss param
 //var launchContextId = "af8b7e6f-07a4-4f31-a208-f16dcaff06ab";
 	
-alert("serviceUri : " +serviceUri);
-alert("launchContextId : " +launchContextId);
+//alert("serviceUri : " +serviceUri);
+//alert("launchContextId : " +launchContextId);
 
 // The scopes that the app will request from the authorization server
 // encoded in a space-separated string:
@@ -44,8 +44,8 @@ var state = Math.round(Math.random()*100000000).toString();
 var launchUri = window.location.protocol + "//" + window.location.host + window.location.pathname;
 var redirectUri = launchUri.replace("launch.html","");
 	
-alert("launchUri : " +launchUri);
-alert("redirectUri : " +redirectUri);
+//alert("launchUri : " +launchUri);
+//alert("redirectUri : " +redirectUri);
 
 // FHIR Service Conformance Statement URL
 var conformanceUri = serviceUri + "/metadata"
