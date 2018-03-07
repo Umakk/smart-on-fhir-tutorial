41101416d28f6f1c65259b4354fa90a7672e7ba0
Umakk <37061283+Umakk@users.noreply.github.com>
2018-03-07 15:36:39 +0530

    Updated example_smart.js

--- example-smart-app/src/js/example-smart.js
+++ example-smart-app/src/js/example-smart.js
@@ -1,107 +1,105 @@
-        // Change this to the ID of the client that you registered with the SMART on FHIR authorization server.
-        var clientId = "f126a734-1a58-4d79-9b0a-ba8a0527e885";
-        
-        // For demonstration purposes, if you registered a confidential client
-        // you can enter its secret here. The demo app will pretend it's a confidential
-        // app (in reality it cannot be confidential, since it cannot keep secrets in the
-        // browser)
-        var secret = null;    // set me, if confidential
-        
-        // These parameters will be received at launch time in the URL
-        //var serviceUri = getUrlParameter("iss");
-        //var launchContextId = getUrlParameter("launch");
-            
-        var serviceUri = "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca";   //iss param
-        var launchContextId = "af8b7e6f-07a4-4f31-a208-f16dcaff06ab";
-            
-        alert("serviceUri : " +serviceUri);
-        alert("launchContextId : " +launchContextId);
-        
-        // The scopes that the app will request from the authorization server
-        // encoded in a space-separated string:
-        //      1. permission to read all of the patient's record
-        //      2. permission to launch the app in the specific context
-        /*var scope = [
-                "patient/*.read",
-                "launch"
-            ].join(" ");*/
-            
-         var scope = [
-                "patient/Patient.read",
-                "patient/Observation.read",
-                "online_access",
-                "openid",
-                "launch",
-                "profile"
-            ].join(" ");
-            
-        // Generate a unique session key string (here we just generate a random number
-        // for simplicity, but this is not 100% collision-proof)
-        var state = Math.round(Math.random()*100000000).toString();
-        
-        // To keep things flexible, let's construct the launch URL by taking the base of the 
-        // current URL and replace "launch.html" with "index.html".
-        var launchUri = window.location.protocol + "//" + window.location.host + window.location.pathname;
-        var redirectUri = launchUri.replace("launch.html","");
-            
-        alert("launchUri : " +launchUri);
-        alert("redirectUri : " +redirectUri);
-        
-        // FHIR Service Conformance Statement URL
-        var conformanceUri = serviceUri + "/metadata"
-        
-        // Let's request the conformance statement from the SMART on FHIR API server and
-        // find out the endpoint URLs for the authorization server
-        $.get(conformanceUri, function(r){
+// Change this to the ID of the client that you registered with the SMART on FHIR authorization server.
+var clientId = "f126a734-1a58-4d79-9b0a-ba8a0527e885";
 
-            var authUri,
-                tokenUri;
-            
-            var smartExtension = r.rest[0].security.extension.filter(function (e) {
-               return (e.url === "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris");
-            });
+// For demonstration purposes, if you registered a confidential client
+// you can enter its secret here. The demo app will pretend it's a confidential
+// app (in reality it cannot be confidential, since it cannot keep secrets in the
+// browser)
+var secret = null;    // set me, if confidential
 
-            smartExtension[0].extension.forEach(function(arg, index, array){
-              if (arg.url === "authorize") {
-                authUri = arg.valueUri;
-              } else if (arg.url === "token") {
-                tokenUri = arg.valueUri;
-              }
-            });
-            
-            // retain a couple parameters in the session for later use
-            sessionStorage[state] = JSON.stringify({
-                clientId: clientId,
-                secret: secret,
-                serviceUri: serviceUri,
-                redirectUri: redirectUri,
-                tokenUri: tokenUri
-            });
+// These parameters will be received at launch time in the URL
+//var serviceUri = getUrlParameter("iss");
+//var launchContextId = getUrlParameter("launch");
+	
+var serviceUri = "https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca";   //iss param
+//var launchContextId = "af8b7e6f-07a4-4f31-a208-f16dcaff06ab";
+	
+alert("serviceUri : " +serviceUri);
 
-            // finally, redirect the browser to the authorizatin server and pass the needed
-            // parameters for the authorization request in the URL
-            window.location.href = authUri + "?" +
-                "response_type=code&" +
-                "client_id=" + encodeURIComponent(clientId) + "&" +
-                "scope=" + encodeURIComponent(scope) + "&" +
-                "redirect_uri=" + encodeURIComponent(redirectUri) + "&" +
-                "aud=" + encodeURIComponent(serviceUri) + "&" +
-                //"launch=" + launchContextId + "&" +
-                "state=" + state;
-         }, "json");
-        
-        // Convenience function for parsing of URL parameters
-        // based on http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
-        function getUrlParameter(sParam)
-        {
-            var sPageURL = window.location.search.substring(1);
-            var sURLVariables = sPageURL.split('&');
-            for (var i = 0; i < sURLVariables.length; i++) 
-            {
-                var sParameterName = sURLVariables[i].split('=');
-                if (sParameterName[0] == sParam) {
-                    var res = sParameterName[1].replace(/\+/g, '%20');
-                    return decodeURIComponent(res);
-                }
-            }
-        }
+// The scopes that the app will request from the authorization server
+// encoded in a space-separated string:
+//      1. permission to read all of the patient's record
+//      2. permission to launch the app in the specific context
+/*var scope = [
+"patient/*.read",
+"launch"
+].join(" ");*/
+	
+ var scope = [
+        "patient/Patient.read",
+        "patient/Observation.read",
+        "online_access",
+        "openid",
+        "launch",
+        "profile"
+].join(" ");
+	
+// Generate a unique session key string (here we just generate a random number
+// for simplicity, but this is not 100% collision-proof)
+var state = Math.round(Math.random()*100000000).toString();
+
+// To keep things flexible, let's construct the launch URL by taking the base of the 
+// current URL and replace "launch.html" with "index.html".
+var launchUri = window.location.protocol + "//" + window.location.host + window.location.pathname;
+var redirectUri = launchUri.replace("launch.html","");
+	
+alert("launchUri : " +launchUri);
+alert("redirectUri : " +redirectUri);
+
+// FHIR Service Conformance Statement URL
+var conformanceUri = serviceUri + "/metadata"
+
+// Let's request the conformance statement from the SMART on FHIR API server and
+// find out the endpoint URLs for the authorization server
+$.get(conformanceUri, function(r){
+
+	var authUri, tokenUri;
+	
+	var smartExtension = r.rest[0].security.extension.filter(function (e) {
+	   return (e.url === "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris");
+	});
+
+	smartExtension[0].extension.forEach(function(arg, index, array){
+	  if (arg.url === "authorize") {
+		authUri = arg.valueUri;
+	  } else if (arg.url === "token") {
+		tokenUri = arg.valueUri;
+	  }
+	});
+	
+	// retain a couple parameters in the session for later use
+	sessionStorage[state] = JSON.stringify({
+		clientId: clientId,
+		secret: secret,
+		serviceUri: serviceUri,
+		redirectUri: redirectUri,
+		tokenUri: tokenUri
+	});
+
+	// finally, redirect the browser to the authorizatin server and pass the needed
+	// parameters for the authorization request in the URL
+	window.location.href = authUri + "?" +
+		"response_type=code&" +
+		"client_id=" + encodeURIComponent(clientId) + "&" +
+		"scope=" + encodeURIComponent(scope) + "&" +
+		"redirect_uri=" + encodeURIComponent(redirectUri) + "&" +
+		"aud=" + encodeURIComponent(serviceUri) + "&" +
+		//"launch=" + launchContextId + "&" +
+		"state=" + state;
+ }, "json");
+
+// Convenience function for parsing of URL parameters
+// based on http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+function getUrlParameter(sParam)
+{
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	for (var i = 0; i < sURLVariables.length; i++) 
+	{
+		var sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] == sParam) {
+			var res = sParameterName[1].replace(/\+/g, '%20');
+			return decodeURIComponent(res);
+		}
+	}
+}
